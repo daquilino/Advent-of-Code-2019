a0414551dcc1b8453b1b5a499ade845de89acfd6
D <douglasjaquilino@gmail.com>
2019-12-04 16:12:29 -0500

    completed day2-1, correct

---
+++ Day-2/day2-1.js
@@ -0,0 +1,35 @@
+const FS = require('fs');
+
+
+let input = FS.readFileSync("input.txt", "utf8").split(",");
+
+//test
+//let input = [1,0,0,0,99]
+
+
+for (let i = 0; i < input.length; i += 4) {
+
+    let command = parseInt(input[i]);
+    let input1 = parseInt(input[input[i + 1]]);
+    let input2 = parseInt(input[input[i + 2]]);
+    let outputIndex = input[i + 3]
+
+
+    switch (command) {
+        case 1:
+            input[outputIndex] = input1 + input2;
+            break;
+        case 2:
+            input[outputIndex] = input1 * input2;
+            break;
+        case 99:
+            console.log(`Value at postion 0: ${input[0]}`);
+            return;
+            break;
+
+        default:
+            break;
+    }
+
+}
+
