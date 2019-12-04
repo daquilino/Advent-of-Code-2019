9bdb5750996d4218ff424114d43386dbc121e3b4
D <douglasjaquilino@gmail.com>
2019-12-04 13:44:07 -0500

    completed day 1 part 2, correct

--- Day-1/day1-2.js
+++ Day-1/day1-2.js
@@ -5,12 +5,20 @@ function calculateFuel() {
 
     // read Module Masses from file
     let moduleMass = FS.readFileSync("input.txt", "utf8").split("\n");
+    
 
-    return moduleMass.reduce((totalFuel, mass) => totalFuel += Math.floor(parseInt(mass) / 3) - 2, 0)
+    return moduleMass.reduce((totalFuel, mass) => {
+        
+        let modFuel = Math.floor(parseInt(mass) / 3) - 2
+        
+        return totalFuel += (modFuel + calcFuelFuel(modFuel));
+    
+    }, 0)
 
 }
 
 
+// recursivly calculates fuel needed given mass of fuel.
 function calcFuelFuel(mass, fuelFuel = 0) {
 
     let fuel = Math.floor(parseInt(mass) / 3) - 2;
@@ -21,6 +29,5 @@ function calcFuelFuel(mass, fuelFuel = 0) {
 
 }
 
-console.log(calcFuelFuel(654));
 
-//console.log(`Total Fuel Needed: ${calculateFuel()}`)
+console.log(`Total Fuel Needed: ${calculateFuel()}`)
