e0498aa68965f45993a86557531ddc4861150812
D <douglasjaquilino@gmail.com>
2019-12-03 19:24:53 -0500

    day-1 part 2 almost done

--- Day-1/day1-2.js
+++ Day-1/day1-2.js
@@ -5,10 +5,22 @@ function calculateFuel() {
 
     // read Module Masses from file
     let moduleMass = FS.readFileSync("input.txt", "utf8").split("\n");
- 
+
     return moduleMass.reduce((totalFuel, mass) => totalFuel += Math.floor(parseInt(mass) / 3) - 2, 0)
- 
+
 }
 
 
-console.log(`Total Fuel Needed: ${calculateFuel()}`)
+function calcFuelFuel(mass, fuelFuel = 0) {
+
+    let fuel = Math.floor(parseInt(mass) / 3) - 2;
+
+    if (fuel <= 0) return fuelFuel;
+
+    return calcFuelFuel(fuel, fuelFuel + fuel);
+
+}
+
+console.log(calcFuelFuel(654));
+
+//console.log(`Total Fuel Needed: ${calculateFuel()}`)
