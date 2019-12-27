15d5d7e3b9f07500584edaea9127edb4c4716903
D <douglasjaquilino@gmail.com>
2019-12-27 15:02:31 -0500

    need to debug issue with loop

--- Day-2/day2-2.js
+++ Day-2/day2-2.js
@@ -1,35 +1,51 @@
 const FS = require('fs');
 
+// read intcode from file, split into array, parseInt array using map
+let input = FS.readFileSync("input-1.txt", "utf8").split(",").map(num => parseInt(num));
 
-let input = FS.readFileSync("input.txt", "utf8").split(",");
 
-console.log(input.slice(0,4));
+function runIntCode(input, noun, verb) {
 
-for (let i = 0; i < input.length; i += 4) {
+    // noun and verb parameter are assigned to respective positions in input intcode array.
+    input[1] = noun;
+    input[2] = verb;
 
-    let command = parseInt(input[i]);
-    let input1 = parseInt(input[input[i + 1]]);
-    let input2 = parseInt(input[input[i + 2]]);
-    let outputIndex = input[i + 3]
+    for (let i = 0; i < input.length; i += 4) {
 
-    switch (command) {
-        case 1:
-            console.log(`adding noun:${input1}  verb:${input2} outputIndex:${outputIndex}`);
-            input[outputIndex] = input1 + input2;
-            break;
-        case 2:
-            console.log(`multiplying noun:${input1}  verb:${input2} outputIndex:${outputIndex}`);
-            input[outputIndex] = input1 * input2;
-            break;
-        case 99:
-            console.log(`Value at postion 0: ${input[0]}`);
-            return;
-            break;
-        default:
-            break;
+        let command = input[i];
+        let input1 = input[input[i + 1]];
+        let input2 = input[input[i + 2]];
+        let outputIndex = input[i + 3]
+
+        switch (command) {
+            case 1:
+                input[outputIndex] = input1 + input2;
+                break;
+            case 2:
+                input[outputIndex] = input1 * input2;
+                break;
+            case 99:
+                console.log(`Value at postion 0: ${input[0]}`);
+                return;
+                break;
+            default:
+                break;
+        }
+
     }
 
-    console.log(input.slice(0,4));
 }
 
+
+function findNounVerb(match){
+
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+            let output = runIntCode(input, i, j);
+            if (output == match) return { i, j };
+        }
+    }
+}
+
+console.log(findNounVerb(5866663));
 
