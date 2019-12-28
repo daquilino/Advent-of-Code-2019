e944eac2ca2081be1ff931539850c2818ecbce7b
D <douglasjaquilino@gmail.com>
2019-12-28 17:14:25 -0500

    pseudo code problem

---
+++ Day-3/day3-1.js
@@ -0,0 +1,7 @@
+/*
+    1. convert the wire paths to point coordinates (x,y values)
+    2. compare the points to determine where the wires cross (they share the same point);
+    3. determine which wire 'cross' is closest to origin (starting point);
+
+
+*/
