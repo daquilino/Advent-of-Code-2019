40b155f10ff942cbb764ca81aef1ea934ff05a0d
D <douglasjaquilino@gmail.com>
2019-12-28 19:54:42 -0500

    started makePoints function

--- Day-3/day3-1.js
+++ Day-3/day3-1.js
@@ -5,3 +5,40 @@
 
 
 */
+
+//test examples
+let wire1 = ["R75","D30","R83","U83","L12","D49","R71","U7","L72"];
+let wire2  = ["U62","R66","U55","R34","D71","R55","D58","R83"];
+
+let wire1Points = [];
+let wire2Points = []
+
+let currentPosition = {x:0, y:0};
+
+function getPoints(val){
+
+     let dir = val.slice(0,1);
+     let steps = parseInt(val.slice(1))   
+
+    switch (dir){
+        case "R":
+           currentPosition.x += steps;
+           break;
+        case "U":
+            currentPosition.y += steps;
+            break;
+        case "D":
+            currentPosition.y -= steps;
+            break;
+        case "L":
+            currentPosition.x -= steps;
+            break;
+    }
+
+
+
+}
+
+getPoints();
+
+console.log(currentPosition);
