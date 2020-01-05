561d4337952417bd7081a11de87e40660f289417
D <douglasjaquilino@gmail.com>
2020-01-04 19:36:04 -0500

    removed test code

--- Day-3/day3-1.js
+++ Day-3/day3-1.js
@@ -101,10 +101,4 @@ function findShortestDistance(wire1,wire2){
 
 }
 
-// let wire1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"];
-// let wire2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"];
-
-// let wire3 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(",");
-// let wire4 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",");
-
 console.log(findShortestDistance(wire1, wire2));
