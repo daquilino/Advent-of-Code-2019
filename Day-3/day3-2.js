04d2ea26f8347591bd510846f91c491e4a179f35
D <douglasjaquilino@gmail.com>
2020-01-20 15:52:37 -0500

    findMinSteps() gives wrong value, need to debug

--- Day-3/day3-2.js
+++ Day-3/day3-2.js
@@ -116,18 +116,21 @@ function manDistFromOrigin(point){
 }
 
 function findMinSteps(junctions){
+    
     let minSteps;
-
-
+    let iterations = 0;   // used to assign first Map value to minSteps
 
     junctions.forEach(function(val){
 
-        
-
-
+        // This assignes a starting value to minSteps.    
+        if(iterations == 0){
+            minSteps = val;
+        }
+        else if(val < minSteps){
+            minSteps = val;
+        }
+    
     })
-
-
     return minSteps;
 
 }
@@ -161,9 +164,8 @@ R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
 */
 
-// let test1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
-// let test2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
+ let test1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
+ let test2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
 
-// console.log("Shortest Manhattan Distance:", findShortestDistance(test1, test2));
+ console.log("Shortest Manhattan Distance:", findShortestDistance(test1, test2));
 
-console.log(eval(4 < 0));
